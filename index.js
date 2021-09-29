8bf9f944b561f71f70eba50cf127c6b3bf15b100
vamandeshmukh <vamandeshmukh@gmail.com>
2021-09-29 12:30:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const app = express();
 const port = 3333;
 
 app.get('/', (req, res) => {
+    console.log("Hello");
     res.send('Hello World!');
-})
+});
+
+app.get('/emp', (req, res) => {
+    console.log("Emp");
+    res.send('Employee data');
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
