d36bbc9386b6ae1539989338c2cdb06876eb56a2
vamandeshmukh <vamandeshmukh@gmail.com>
2021-09-29 21:37:03 +0530

    Commit
    Commit

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3333;
 
+let employee = {
+    firstName: "Sonu",
+    phone: 9876543210,
+    city: "Hyderabad"
+};
+
 app.get('/', (req, res) => {
     console.log("Hello");
     res.send('Hello World!');
@@ -9,7 +15,7 @@ app.get('/', (req, res) => {
 
 app.get('/emp', (req, res) => {
     console.log("Emp");
-    res.send('Employee data');
+    res.send(employee);
 });
 
 app.listen(port, () => {
